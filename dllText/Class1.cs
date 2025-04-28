1a8cd86769bdfe08ee0254e80c852c526e42903c
жамалетдиноврм <@105-05>
2025-04-28 10:20:20 +0300

    add kod for task reverse

--- dllText/Class1.cs
+++ dllText/Class1.cs
@@ -2,6 +2,15 @@
 {
     public class Class
     {
-        
+        public static string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+
+        }
     }
 }
