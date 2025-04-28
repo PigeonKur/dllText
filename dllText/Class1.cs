fe50d7b77ccf01b99016c130b71b6778ee28ebbf
PigeonKur <ggnn3974@gmail.com>
2025-04-28 10:13:44 +0300

    complete task 'Convert_lowercase_uppercase'

--- dllText/Class1.cs
+++ dllText/Class1.cs
@@ -2,6 +2,20 @@
 {
     public class Class
     {
-        
+        public static string TransformTextCase(string text, string mode)
+        {
+            switch (mode.ToLower())
+            {
+                case "lower":
+                    return text.ToLower();
+                case "upper":
+                    return text.ToUpper();
+                case "title":
+                    return System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text.ToLower());
+                default:
+                    throw new ArgumentException("Недопустимый режим. Используйте 'lower', 'upper' или 'title'.");
+            }
+        }
+
     }
 }
